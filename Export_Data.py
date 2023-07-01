63dede8e91bdb9ab8e2271338c6c13ace1123c34
MrBeyondBIM <mr@beyondbim.ch>
2023-07-01 08:03:33 +0200

    First Version

---
+++ Export_Data.py
@@ -0,0 +1,5 @@
+class Exp_Holder():
+    def __init__(self, branch, data):
+        self.branch = branch
+        self.data = data
+
---
+++ Helpers.py
@@ -0,0 +1,14 @@
+#Class Data Holder Config
+
+class Data_Holder():
+    def __init__(self, category, prop_list):
+        self.category = category
+        self.prop_list = prop_list
+
+		
+class Prop_Holder():
+    def __init__(self, pset, prop):
+        self.pset = pset
+        self.prop = prop
+
+		
